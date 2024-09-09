3d5fe94c65a2a712d64c437bbe5015194f1a354d
mostermouse <whdls7946@gmail.com>
2024-09-09 16:20:20 +0900

    타임리프 이용

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,6 +25,15 @@ public class BasicItemController {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items" , items);
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(
+            //특정 자원 조회할때 사용하고 여러자원은 리퀘스트파람 사용
+            @PathVariable("itemId")  long itemId , Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item" ,item);
+        return "basic/item";
     }
 
     /*테스트 용*/
